df7fd153b43bc707b3d36c5a8ac5fc92c77e6574
斌斌斌 <1587443838@qq.com>
2020-02-26 18:16:06 +0800

    DOM DIFF

--- src/diff.js
+++ src/diff.js
@@ -23,19 +23,48 @@ function diffAttr(oldAttrs,newAttrs) {
     return patch;
 }
 const ATTRS = 'ATTRS';
+const TEXT = 'TEXT';
+const REMOVE = 'REMOVE';
+const REPLACE = 'REPLACE'
+let Index = 0;
+function diffChildren(oldChildren,newChildren,index,patches) {
+    //比较老的第一个和新的第一个
+    oldChildren.forEach((child,idx) => {
+        //索引不应该是index
+        //index 每次传递给walk时 index是递增的 所有的人都基于一个序号来实现
+        walk(child,newChildren[idx],++Index,patches)
+    })
+}
+function isString(node) {
+    return Object.prototype.toString.call(node) === '[object String]'
+}
+//index被私有化到了walk作用域内
 function walk(oldNode,newNode,index,patches) {
-    let currentPatch = [];
-    if(oldNode.type === newNode.type){
+    let currentPatch = []; //每个元素都有一个补丁对象
+    if(!newNode){
+        currentPatch.push({type:REMOVE,index})
+    }else if(isString(oldNode) && isString(newNode)){//判断文本是否一致
+        if(oldNode !== newNode){
+            currentPatch.push({type:TEXT,text:newNode})
+        }
+    }else if(oldNode.type === newNode.type){
         //比较属性是否有更改
        let attrs =  diffAttr(oldNode.props,newNode.props);
        //判断对象有没有值
         if(Object.keys(attrs).length>0){
             currentPatch.push({type:ATTRS,attrs})
         }
+        //如果有儿子节点 遍历儿子
+        diffChildren(oldNode.children,newNode.children,index,patches);
+
+    }else{
+        //说明节点被替换了 如li 变成div了
+        currentPatch.push({type:REPLACE,newNode})
     }
     if(currentPatch.length > 0){//当前元素确实有补丁
         //将元素和补丁对应起来 放到大补丁包中
         patches[index] = currentPatch;
+        console.log(patches,'===patches');
     }
 
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {createElement,render,renderDom} from './element'
 import diff from './diff'
+import patch from './patch'
 let vertualDom = createElement('ul',{class:'list'},[
     createElement('li',{class:'item'},['a']),
     createElement('li',{class:'item'},['b']),
@@ -8,19 +9,28 @@ let vertualDom = createElement('ul',{class:'list'},[
 let vertualDom2 = createElement('ul',{class:'list-group'},[
     createElement('li',{class:'item'},['1']),
     createElement('li',{class:'item'},['b']),
-    createElement('li',{class:'item'},['3'])
+    createElement('div',{class:'item'},['3'])
 ]);
-//将虚拟dom转化成了真实dom渲染到页面上
+
+//存在小问题 如果平级元素有互换 那会导致重新渲染
+//新增节点也不会被更新
+//index
+
 let el = render(vertualDom);
 renderDom(el,window.root);
+let patches = diff(vertualDom,vertualDom2);
+//给元素打补丁 重新更新视图
+patch(el,patches);
+
+
+//将虚拟dom转化成了真实dom渲染到页面上
+// let el = render(vertualDom);
+// renderDom(el,window.root);
 console.log(el);
 console.log(vertualDom);
 
 //DOM DIFF 比价两个虚拟DOM区别 比较两个对象的区别
 // dom diff 作用 根据两个虚拟对象创建出补丁，描述下改变的内容，
 //将这个补丁用来更新dom
-
-let patches = diff(vertualDom,vertualDom2);
-
 
 
